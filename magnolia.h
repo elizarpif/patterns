e22df586622145cfb5e16e1be856450042c02044
elizarpif <liza.pif@mail.ru>
2021-03-23 01:39:45 +0300

    abstract factory, singleton

---
+++ magnolia.h
@@ -0,0 +1,4 @@
+#ifndef MAGNOLIA_H
+#define MAGNOLIA_H
+
+#endif // MAGNOLIA_H
---
+++ product.h
@@ -0,0 +1,11 @@
+#ifndef PRODUCT_H
+#define PRODUCT_H
+
+
+class product
+{
+public:
+    product();
+};
+
+#endif // PRODUCT_H
